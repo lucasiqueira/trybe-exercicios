4179c14e8e75e843b331ae09174fac4e6dd4fcd9
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-11-23 15:23:59 -0300

    Aplicação de exercícios da seção Variáveis

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,15 @@
+const myName = 'Lucas';
+const birthCity = 'Goiânia';
+let birthYear = 1994;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// birthCity = 'São Paulo'; Isso não funciona, pois está atribuindo um novo valor a uma variável constante.
+
+// console.log(birthCity);
