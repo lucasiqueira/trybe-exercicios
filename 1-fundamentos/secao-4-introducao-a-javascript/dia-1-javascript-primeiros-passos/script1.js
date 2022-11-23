576ca8a5f7026436668dd1f6e25c629fdb304082
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-11-23 18:16:30 -0300

    Inserção de acréscimo e decréscimo de um nas variáveis

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script1.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script1.js
@@ -3,3 +3,11 @@ let score = 10.89; //number literal
 let isValid = true; //boolean
 let name; //undefined
 let color = null; //aguarda redefinição
+
+score++;
+
+console.log(score);
+
+score--;
+
+console.log(score);
