618004f89692eb50d1e06c21fb2cd8c7267136de
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-11-23 21:38:54 -0300

    Exercícios de par ou ímpar

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/par-ou-impar.js
@@ -0,0 +1,21 @@
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+const a = 4;
+const b = 2;
+const c = 162;
+
+if ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+if ((a % 2 == 1) || (b % 2 == 1) || (c % 2 == 1)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
