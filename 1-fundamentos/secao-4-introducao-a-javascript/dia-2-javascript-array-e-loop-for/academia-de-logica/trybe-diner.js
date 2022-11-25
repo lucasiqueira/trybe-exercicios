9b30be4bdba09d7d0a27a015f3e07c5ec4c8ab09
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-11-25 15:37:51 -0300

    Exercício 'Lanchonete da Trybe' da Academia de Lógica

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/academia-de-logica/trybe-diner.js
@@ -0,0 +1,21 @@
+let item = 3;
+
+switch (item) {
+  case 1:
+    console.log("Trybe Lanche Feliz");
+    break;
+  case 2:
+    console.log('McTrybe');
+    break;
+  case 3:
+    console.log('TrybeWooper');
+    break;
+  case 4:
+    console.log('X-Trybe');
+    break;
+  case 5:
+    console.log('Triplo Trybe com JS');
+    break;
+  default:
+    console.log("Ainda não possuímos esta opção.");
+}
