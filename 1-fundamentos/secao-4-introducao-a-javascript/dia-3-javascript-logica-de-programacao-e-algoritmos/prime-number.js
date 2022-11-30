4018639019abd826bbd165921272c2b4aaba7eef
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-11-29 21:13:36 -0300

    Finalização do exercício de maior número primo

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/prime-number.js
@@ -0,0 +1,17 @@
+let maximumVerified = 50;
+let counter = 0;
+let array = [];
+
+for (let index = 2; index <= maximumVerified; index += 1) {
+  for (let divisor = 2; divisor <= index; divisor += 1) {
+    if (index % divisor == 0) {
+      counter += 1;
+    }
+  }
+  if (counter === 1) {
+    array.push(index);
+  }
+  counter = 0;
+}
+
+console.log('Maior número primo entre 2 e', maximumVerified, ':', array[array.length - 1]);
