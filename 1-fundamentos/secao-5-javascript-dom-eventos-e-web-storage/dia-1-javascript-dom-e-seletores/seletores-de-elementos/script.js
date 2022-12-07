b6292274812d06ac2fcc4c8e015f733bac3baffb
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-12-07 18:38:19 -0300

    feat: arquivos index.html e script.js finalizados

---
+++ 1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/script.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Harry Potter and the Prisoner of Azkaban';
+
+document.getElementById('second-paragraph').innerText = 'Brasil é HEXA!';
+
+document.getElementById('subtitle').innerText = 'Viva Dom Marcel!!';
