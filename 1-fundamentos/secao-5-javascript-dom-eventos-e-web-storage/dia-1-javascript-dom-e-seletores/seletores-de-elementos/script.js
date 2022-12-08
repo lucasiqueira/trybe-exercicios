f304060f2cac9d4586ff3357888b3f3ec9115d98
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-12-07 21:58:06 -0300

    tests: finalização da página "seletores de elementos"

--- 1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/script.js
+++ 1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/script.js
@@ -6,3 +6,12 @@ document.getElementById('page-title').innerText = 'Harry Potter and the Prisoner
 document.getElementById('second-paragraph').innerText = 'Brasil é HEXA!';
 
 document.getElementById('subtitle').innerText = 'Viva Dom Marcel!!';
+
+const texts = document.getElementsByClassName('texts');
+console.log(texts);
+texts[0].style.color = 'purple';
+texts[0].style.backgroundColor = 'aquamarine';
+
+const subtitle = document.getElementsByTagName('h4');
+subtitle[0].style.color = 'green';
+console.log(subtitle);
