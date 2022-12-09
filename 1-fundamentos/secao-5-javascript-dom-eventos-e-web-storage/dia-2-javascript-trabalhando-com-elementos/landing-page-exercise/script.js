4795377623ca691671425223d6ec67d556995414
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2022-12-08 22:31:40 -0300

    feat: adicionando novos elementos na pagina e editando elementos existentes

--- 1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/landing-page-exercise/script.js
+++ 1-fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/landing-page-exercise/script.js
@@ -8,7 +8,6 @@ Para dar início a página, você deve criar algumas tags:*/
 const body = document.getElementById('body');
 const title = document.createElement('h1');
 title.innerHTML = 'TrybeTrip - Agência de Viagens';
-title.className = 'title';
 body.appendChild(title);
 
 // Adicione a tag main com a classe main-content como filho da tag body;
@@ -51,6 +50,7 @@ rightContent.appendChild(numberList);
 for (let index = 0; index < numbers.length; index += 1) {
   const listItem = document.createElement('li');
   listItem.className = 'number';
+  listItem.id = numbers[index];
   listItem.innerHTML = numbers[index];
   numberList.appendChild(listItem);
 }
@@ -58,6 +58,30 @@ for (let index = 0; index < numbers.length; index += 1) {
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 for (let index = 0; index < 3; index += 1) {
   const h3 = document.createElement('h3');
-  h3.className = 'main-h3';
   mainContent.appendChild(h3);
 }
+
+/* Após criar as tags anteriores, você mostrou para a sua liderança como estava a estrutura inicial da página. Sua liderança então pediu para que você fizesse algumas alterações: */
+
+// Adicione a classe title na tag h1 criada;
+title.className = 'title';
+
+// Adicione a classe description nas 3 tags h3 criadas;
+const h3 = document.getElementsByTagName('h3');
+for (let index = 0; index < h3.length; index += 1) {
+  h3[index].className = 'description';
+}
+
+// Remova a section criada no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild();
+mainContent.removeChild(leftContent);
+
+/* Centralize a section criada no passo 6 (aquele que possui a classe right-content).
+De olho na dica 👀:: Para centralizar a section, basta configurar o margin-right: auto da section; */
+rightContent.style.marginRight = 'auto';
+
+// Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
+mainContent.style.backgroundColor = 'green';
+
+// Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
+numberList.removeChild(document.getElementById('nove'));
+numberList.removeChild(document.getElementById('dez'));
