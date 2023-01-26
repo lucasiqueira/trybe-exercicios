5bbf25f34ec9fb49c7b18b26225b04a7fa355611
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-26 08:23:59 -0300

    funcao hof newemployees e generateemployee implementadas

---
+++ 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercise/newEmployees.js
@@ -0,0 +1,22 @@
+/* Nova pessoa contratada
+Você ficou responsável por organizar o sistema de contratação de uma empresa e deve criar uma estrutura de dados que possua o formato { nomeCompleto, email } para representar a nova pessoa contratada. Além disso, você deve gerar os emails de cada pessoa que é contratada na empresa. Para isso:
+
+crie uma função que receba como parâmetro o nome completo da nova pessoa colaboradora e que automaticamente gere um email no formato nome_da_pessoa@trybe.com;
+
+utilize a função newEmployees, definida abaixo, para receber a sua função como parâmetro e criar o objeto de cada pessoa contratada. */
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const generateEmployee = (fullName) => {
+  let email = fullName.toLowerCase().replace(' ', '_');
+  return {fullName, email: `${email}@trybe.com`};
+};
+
+console.log(newEmployees(generateEmployee));
