406f7c86f00e90102120f25a33e9fbf80c46d819
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-27 15:06:23 -0300

    exercise: funcoes gettotalarea e getpopulation implementadas

--- 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercise/bonus/exercise1.js
+++ 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercise/bonus/exercise1.js
@@ -1,68 +1,11 @@
-// Fonte: <https://restcountries.com/v2/all>
+const countries = require('./database/countries');
 
-module.exports = [
-  {
-    name: 'Afghanistan',
-    region: 'Asia',
-    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
-    capital: 'Kabul',
-    population: 40218234,
-    area: 652230,
-  },
-  {
-    name: 'Åland Islands',
-    region: 'Europe',
-    currencies: [{ code: 'EUR', name: 'Euro' }],
-    capital: 'Mariehamn',
-    population: 28875,
-    area: 1580,
-  },
-  {
-    name: 'Albania',
-    region: 'Europe',
-    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
-    capital: 'Tirana',
-    population: 2837743,
-    area: 28748,
-  },
-  {
-    name: 'Algeria',
-    region: 'Africa',
-    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
-    capital: 'Algiers',
-    population: 44700000,
-    area: 2381741,
-  },
-  {
-    name: 'American Samoa',
-    region: 'Oceania',
-    currencies: [{ code: 'USD', name: 'United States Dollar' }],
-    capital: 'Pago Pago',
-    population: 55197,
-    area: 199,
-  },
-  {
-    name: 'Andorra',
-    region: 'Europe',
-    currencies: [{ code: 'EUR', name: 'Euro' }],
-    capital: 'Andorra la Vella',
-    population: 77265,
-    area: 468,
-  },
-  {
-    name: 'Angola',
-    region: 'Africa',
-    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
-    capital: 'Luanda',
-    population: 32866268,
-    area: 1246700,
-  },
-  {
-    name: 'Anguilla',
-    region: 'Americas',
-    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
-    capital: 'The Valley',
-    population: 13452,
-    area: 91,
-  },
-];
+// 1 - Calcule a quantidade total da população de todos os países.
+// De olho na dica 👀: o valor inicial deve começar com 0, portanto o acumulador é um número.
+
+// const expectedResult = 120797034;
+
+const getPopulation = (data) => data.reduce((acc, curr) => acc + curr.population, 0);
+
+// console.log(getPopulation(countries));
+getPopulation(countries);
