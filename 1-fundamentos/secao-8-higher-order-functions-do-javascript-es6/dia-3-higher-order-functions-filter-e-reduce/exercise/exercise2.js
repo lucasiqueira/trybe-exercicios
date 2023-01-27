2679a8c4dd444daf8ad31f2dafa6cd069532c629
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-27 14:12:16 -0300

    exercise: manipulacao de biblioteca - inicio

---
+++ 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercise/exercise2.js
@@ -0,0 +1,12 @@
+const books = require('../../dia-1-introducao-a-higher-order-functions/exercise/library-management/database/books');
+
+/* 🚀 2 - Filtre os livros com mais de 60 anos desde sua
+publicação e ordene a partir do livro mais velho para o mais novo. */
+const currYear = 2023;
+
+const getAlmostPublicDomainBooks = (data) => data.filter((bk) => (currYear - bk.releaseYear) > 60)
+  .sort((a, b) => a.releaseYear - b.releaseYear);
+
+// console.log(getAlmostPublicDomainBooks(books));
+
+getAlmostPublicDomainBooks(books);
