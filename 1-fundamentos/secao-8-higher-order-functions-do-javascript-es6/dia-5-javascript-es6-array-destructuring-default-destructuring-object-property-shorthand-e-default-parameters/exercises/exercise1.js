e2157bd65821cb2749fc28540c77c6f1683361a1
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-30 18:53:57 -0300

    exercise: funcao toObject implementada

--- 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercises/exercise1.js
+++ 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercises/exercise1.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
-/* Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5]. */
+/* Exercício 1
+Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5]. */
 
 const myList = [5, 2, 3];
 
