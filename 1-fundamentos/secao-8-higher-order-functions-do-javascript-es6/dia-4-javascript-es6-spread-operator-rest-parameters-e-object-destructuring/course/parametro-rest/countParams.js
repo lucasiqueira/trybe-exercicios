155e71fe0f7076985cc3cf30fbbed552bee51720
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-30 12:23:11 -0300

    exercises: secao sobre o parametro rest

---
+++ 1-fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/course/parametro-rest/countParams.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const countParams = (...args) => {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+};
+
+console.log(countParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(countParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
