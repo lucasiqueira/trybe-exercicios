8bb9e5f602ff09b2fa19a5c533200ceea0650728
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-31 21:58:19 -0300

    feat: aplicacao gerador de senhas com funcoes finalizadas

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,2 +1,9 @@
-/* eslint-disable no-console */
-console.log('Hello World');
+/* eslint-disable import/no-extraneous-dependencies */
+import { nanoid } from 'nanoid';
+
+const passwordButton = document.querySelector('button');
+const passwordDisplay = document.querySelector('h2');
+
+passwordButton.addEventListener('click', () => {
+  passwordDisplay.innerHTML = nanoid();
+});
