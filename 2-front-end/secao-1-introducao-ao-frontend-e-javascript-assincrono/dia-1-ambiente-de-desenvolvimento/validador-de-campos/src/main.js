42a09527029de04ac254b65f943c9e95233968e4
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-01 07:16:09 -0300

    feat: validador de campos finalizado

---
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/validador-de-campos/src/main.js
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import validator from 'validator';
+import './style.css';
+
+const btn = document.querySelector('button');
+const combo = document.getElementById('combo');
+const input = document.querySelector('input');
+const result = document.querySelector('p');
+
+const validateEmail = () => {
+  const validation = validator.isEmail(input.value);
+  result.innerHTML = `O retorno do validador foi ${validation}`;
+};
+
+const validateCpf = () => {
+  const validation = validator.isLength(input.value, { min: 11, max: 11 });
+  result.innerHTML = `O retorno do validador foi ${validation}`;
+};
+
+const validateDate = () => {
+  const validation = validator.isDate(input.value, {
+    format: 'DD/MM/YYYY',
+    strictMode: true,
+  });
+  result.innerHTML = `O retorno do validador foi ${validation}`;
+};
+
+const validateHex = () => {
+  const validation = validator.isHexadecimal(input.value);
+  result.innerHTML = `O retorno do validador foi ${validation}`;
+};
+
+const validateStrongPassword = () => {
+  const validation = validator.isStrongPassword(input.value);
+  result.innerHTML = `O retorno do validador foi ${validation}`;
+};
+
+const validate = () => {
+  if (combo.value === 'email') return validateEmail();
+  if (combo.value === 'cpf') return validateCpf();
+  if (combo.value === 'date') return validateDate();
+  if (combo.value === 'hex') return validateHex();
+  return validateStrongPassword();
+};
+
+btn.addEventListener('click', () => {
+  validate();
+});
