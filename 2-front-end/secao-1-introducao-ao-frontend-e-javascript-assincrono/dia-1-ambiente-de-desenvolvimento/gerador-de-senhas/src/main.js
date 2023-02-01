df1b6b1e0e4695157f409fc9576677d8e2c7ac15
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-01-31 22:33:16 -0300

    feat: copia a senha gerada caso clicada

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordButton = document.querySelector('button');
 const passwordDisplay = document.querySelector('h2');
 
 passwordButton.addEventListener('click', () => {
   passwordDisplay.innerHTML = nanoid();
+});
+
+passwordDisplay.addEventListener('click', () => {
+  copy(passwordDisplay.innerHTML);
 });
