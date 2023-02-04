ac55a9c5dc5420a27fa9d8f42f1c20cda6b11dd6
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-04 11:02:51 -0300

    exercise: metodo finally
    foi adicionado o metodo finally a execucao da promise para verificacao se o codigo seria executado nos dois cenarios de rejected e resolved

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/course/promises/script.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/course/promises/script.js
@@ -50,3 +50,10 @@ randomPromise()
   .catch(error => {
     console.log(`Promise rejeitada: ${error.message}`);
   })
+  .finally(() => {
+    console.log('Fim da execução do promise.');
+  })
+
+console.log(
+  `Esse número não foi gerado por um código assíncrono: ${generateRandomNumber()}`
+);
