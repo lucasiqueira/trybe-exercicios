cd2aa7f4855e75bf2d2e2b0aa40cbcef287dfd42
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-09 23:28:09 -0300

    test: finalizacao de implementacao de testes da funcao getAdress

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/getAddress.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/getAddress.js
@@ -1,6 +1,5 @@
 export default async function getAddress (cep) {
   if (!cep) throw new Error('CEP não foi digitado');
-  console.log(cep);
   const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
   const data = await response.json();
   return data
--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/getAddress.test.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/getAddress.test.js
@@ -14,12 +14,22 @@ describe('Quando a requisição é bem sucedida, ela deverá retornar os dados e
     expect(address.bairro).toBe('Vila Alvorada');
     expect(address.uf).toBe('GO');
   });
+  it('A função deverá aceitar cep com hífen (”74315-400”) ou sem hífen (”74315400”)', async () => {
+    const address1 = await getAddress('74315-400');
+    expect(address1.cep).toBe('74315-400');
+    const address2 = await getAddress('74315400');
+    expect(address2.cep).toBe('74315-400');
+  })
 });
 
-// Quando a requisição é bem sucedida, ela deverá retornar os dados esperados.
-// Os dados devem ser retornados em um objeto;
-// O objeto deve, obrigatoriamente, possuir as chaves logradouro, bairro e uf;
-// A função deverá aceitar cep com hífen (”30130-010”) ou sem hífen (”30130010”).
-// Quando a requisição é rejeitada, ela deverá retornar os erros esperados.
-// Ao passar um cep vazio, a aplicação deverá lançar um erro com a seguinte mensagem: “Você precisa passar um CEP”.
-// Ao passar um cep inválido, a aplicação deverá lançar um erro genérico.
+describe('Quando a requisição é rejeitada, ela deverá retornar os erros esperados', () => {
+  it('Ao passar um cep vazio, a aplicação deverá lançar um erro com a seguinte mensagem: “CEP não foi digitado”', async () => {
+    const expectedError = new Error('CEP não foi digitado');
+    await expect(getAddress('')).rejects.toThrow(expectedError);
+  })
+  it('Ao passar um cep inválido, a aplicação deverá lançar um erro genérico', async () => {
+    await expect(getAddress('00000-00')).rejects.toThrow();
+    await expect(getAddress('00000-0000')).rejects.toThrow();
+    await expect(getAddress('CEP da minha casa')).rejects.toThrow();
+  })
+})
