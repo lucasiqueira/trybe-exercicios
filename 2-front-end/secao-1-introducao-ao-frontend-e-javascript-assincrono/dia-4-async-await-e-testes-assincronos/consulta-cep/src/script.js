d4acb3d80d2e7d735ed9c9dcc396516e610c1974
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-09 21:26:17 -0300

    perf: tratamento de erro inserido na consulta

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/script.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/script.js
@@ -5,7 +5,11 @@ const consultButton = document.getElementById('consult-btn');
 const dataPreview = document.getElementById('data-preview');
 
 consultButton.addEventListener('click', async () => {
-  const response = await fetch(`https://viacep.com.br/ws/${inputCEP.value}/json/`);
-  const data = await response.json();
-  dataPreview.innerHTML = `${data.logradouro}, Bairro: ${data.bairro}, ${data.localidade} - ${data.uf}`;
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${inputCEP.value}/json/`);
+    const data = await response.json();
+    dataPreview.innerHTML = `${data.logradouro}, Bairro: ${data.bairro}, ${data.localidade} - ${data.uf}`;
+  } catch (error) {
+    dataPreview.innerHTML = error;
+  }
 });
