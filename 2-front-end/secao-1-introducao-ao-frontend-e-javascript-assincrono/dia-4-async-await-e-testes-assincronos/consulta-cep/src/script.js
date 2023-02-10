3291fd267878b88664a5e9ec721cb2040d12ba10
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-09 21:23:37 -0300

    feat: implementacao de busca de cep

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/script.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-4-async-await-e-testes-assincronos/consulta-cep/src/script.js
@@ -2,4 +2,10 @@ import './style.css';
 
 const inputCEP = document.getElementById('input-cep');
 const consultButton = document.getElementById('consult-btn');
-const data = document.getElementById('data');
+const dataPreview = document.getElementById('data-preview');
+
+consultButton.addEventListener('click', async () => {
+  const response = await fetch(`https://viacep.com.br/ws/${inputCEP.value}/json/`);
+  const data = await response.json();
+  dataPreview.innerHTML = `${data.logradouro}, Bairro: ${data.bairro}, ${data.localidade} - ${data.uf}`;
+});
