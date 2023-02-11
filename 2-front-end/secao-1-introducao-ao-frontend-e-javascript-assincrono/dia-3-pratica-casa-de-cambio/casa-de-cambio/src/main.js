4b816e65250b7c34b8de017a40b48cf9903907c3
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-10 22:01:09 -0300

    style: responsividade mobile implementada

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/main.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/main.js
@@ -3,6 +3,8 @@ import './style.css';
 const coinInput = document.getElementById('coin-input');
 const searchButton = document.getElementById('search-btn');
 const previewResult = document.getElementById('preview-result');
+const previewTitle = document.getElementById('preview-title');
+const previewCoinData = document.getElementById('preview-coin-data');
 
 const baseURL = 'https://api.exchangerate.host/latest?base=';
 
@@ -14,18 +16,20 @@ const createCoinElement = (coin, data) => {
   const coinRate = document.createElement('p');
   coinContainer.classList.add('coin-container');
   coinDiv.className = 'coin-div';
-  previewResult.appendChild(coinContainer);
+  previewCoinData.appendChild(coinContainer);
   coinContainer.appendChild(coinDiv);
   coinContainer.appendChild(coinRate);
   coinDiv.appendChild(coinImg);
   coinDiv.appendChild(coinTitle);
   coinTitle.innerHTML = coin;
   coinRate.innerHTML = data.rates[coin].toFixed(2);
-  coinImg.src = '.src/assets/imgs/coins.svg';
+  coinImg.src = './src/assets/imgs/coins.svg';
 };
 
 const generateBoard = (data) => {
+  previewResult.style.width = 'auto';
   const coinArray = Object.keys(data.rates);
+  previewTitle.innerHTML = `Valores referentes a 1 ${data.base}`;
   coinArray.forEach((coin) => {
     createCoinElement(coin, data);
   });
@@ -35,6 +39,7 @@ const runCoin = async () => {
   const selectedCoin = coinInput.value;
   const response = await fetch(`${baseURL}${selectedCoin}`);
   const data = await response.json();
+  previewCoinData.innerHTML = '';
   generateBoard(data);
 };
 
