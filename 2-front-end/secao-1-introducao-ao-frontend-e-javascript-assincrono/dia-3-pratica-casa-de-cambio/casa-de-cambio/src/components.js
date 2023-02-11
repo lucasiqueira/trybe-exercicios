3923bc504bc577290dc312bdbe06677d48537ed6
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-10 22:34:06 -0300

    refactor: separacao de funcoes no arquivo components

--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/components.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/components.js
@@ -1,17 +1,26 @@
-export default function createCoinElement(coin, rates) {
+const previewCoinData = document.getElementById('preview-coin-data');
+
+export function createCoinElement(coin, rates) {
   const coinContainer = document.createElement('section');
+  coinContainer.className = 'coin-container';
+  previewCoinData.appendChild(coinContainer);
   const coinDiv = document.createElement('div');
-  const coinImg = document.createElement('img');
-  const coinTitle = document.createElement('p');
-  const coinRate = document.createElement('p');
-  coinContainer.classList.add('coin-container');
   coinDiv.className = 'coin-div';
   coinContainer.appendChild(coinDiv);
-  coinContainer.appendChild(coinRate);
-  coinDiv.appendChild(coinImg);
-  coinDiv.appendChild(coinTitle);
-  coinTitle.innerHTML = coin;
-  coinRate.innerHTML = rates[coin].toFixed(2);
+  const coinImg = document.createElement('img');
   coinImg.src = './src/assets/imgs/coins.svg';
-  return coinContainer;
+  coinDiv.appendChild(coinImg);
+  const coinTitle = document.createElement('p');
+  coinTitle.innerHTML = coin;
+  const coinRate = document.createElement('p');
+  coinRate.innerHTML = rates[coin].toFixed(2);
+  coinContainer.appendChild(coinRate);
+  coinDiv.appendChild(coinTitle);
+}
+
+export default function generateCoinBoard(data) {
+  const coinArray = Object.keys(data.rates);
+  coinArray.forEach((coin) => {
+    createCoinElement(coin, data.rates);
+  });
 }
--- 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/main.js
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/casa-de-cambio/src/main.js
@@ -1,3 +1,4 @@
+import generateCoinBoard from './components';
 import './style.css';
 
 const coinInput = document.getElementById('coin-input');
@@ -7,38 +8,13 @@ const previewCoinData = document.getElementById('preview-coin-data');
 
 const baseURL = 'https://api.exchangerate.host/latest?base=';
 
-const createCoinElement = (coin, data) => {
-  const coinContainer = document.createElement('section');
-  coinContainer.className = 'coin-container';
-  previewCoinData.appendChild(coinContainer);
-  const coinDiv = document.createElement('div');
-  coinDiv.className = 'coin-div';
-  coinContainer.appendChild(coinDiv);
-  const coinImg = document.createElement('img');
-  coinImg.src = './src/assets/imgs/coins.svg';
-  coinDiv.appendChild(coinImg);
-  const coinTitle = document.createElement('p');
-  coinTitle.innerHTML = coin;
-  const coinRate = document.createElement('p');
-  coinRate.innerHTML = data.rates[coin].toFixed(2);
-  coinContainer.appendChild(coinRate);
-  coinDiv.appendChild(coinTitle);
-};
-
-const generateBoard = (data) => {
-  const coinArray = Object.keys(data.rates);
-  previewTitle.innerHTML = `Valores referentes a 1 ${data.base}`;
-  coinArray.forEach((coin) => {
-    createCoinElement(coin, data);
-  });
-};
-
 const runCoin = async () => {
   const selectedCoin = coinInput.value;
   const response = await fetch(`${baseURL}${selectedCoin}`);
   const data = await response.json();
   previewCoinData.innerHTML = '';
-  generateBoard(data);
+  previewTitle.innerHTML = `Valores referentes a 1 ${data.base}`;
+  generateCoinBoard(data);
 };
 
 searchButton.addEventListener('click', runCoin);
