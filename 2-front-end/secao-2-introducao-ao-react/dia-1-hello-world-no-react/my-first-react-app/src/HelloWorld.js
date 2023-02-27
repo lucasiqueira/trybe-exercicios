68cd805181941978d1e83058d448ea3c34b9bf83
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-27 19:17:23 -0300

    fix: alteracao no exercicio hello world

--- 2-front-end/secao-2-introducao-ao-react/dia-1-hello-world-no-react/my-first-react-app/src/HelloWorld.js
+++ 2-front-end/secao-2-introducao-ao-react/dia-1-hello-world-no-react/my-first-react-app/src/HelloWorld.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    return <span>Hello, World!</span>
+    return <span>Hello, World! My name is Lucas. I'm from Brazil.</span>
   }
 }
 
