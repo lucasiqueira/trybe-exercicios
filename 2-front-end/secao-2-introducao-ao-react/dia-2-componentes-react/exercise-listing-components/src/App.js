a9af5b7ede9dca0248d0b8fa3360dc23e92d777d
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-02-28 20:57:28 -0300

    exercise: listing-components finalizado

--- 2-front-end/secao-2-introducao-ao-react/dia-2-componentes-react/exercise-listing-components/src/App.js
+++ 2-front-end/secao-2-introducao-ao-react/dia-2-componentes-react/exercise-listing-components/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
+import UserProfile from './components/UserProfile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const users = [
+      {
+        id: 102,
+        name: 'João',
+        email: 'joao@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png'
+      },
+      {
+        id: 77,
+        name: 'Amélia',
+        email: 'amelia@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png'
+      },
+    ];
+
+    return (
+      <div className="App">
+        {users.map((user) => <UserProfile user={ user } key={ user.id }/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
