f3f885ef4e3c6d90185cbd08757023e2f7f10daf
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-02 16:02:40 -0300

    refactor: refactoring using constructor and this

--- 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Body.jsx
+++ 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Body.jsx
@@ -1,22 +1,29 @@
 import { Component } from "react";
 
-const handlerClick1 = () => {
-  console.log('Botão1');
-}
-const handlerClick2 = () => {
-  console.log('Botão2');
-}
-const handlerClick3 = () => {
-  console.log('Botão3');
-}
 
 class Body extends Component {
+  constructor() {
+    super();
+    this.handlerClick1 = this.handlerClick1.bind(this);
+    this.handlerClick2 = this.handlerClick2.bind(this);
+    this.handlerClick3 = this.handlerClick3.bind(this);
+  }
+
+  handlerClick1() {
+    console.log('Button 1 clicked!');
+  }
+  handlerClick2() {
+    console.log('Button 2 clicked!');
+  }
+  handlerClick3() {
+    console.log('Button 3 clicked!');
+  }
   render() {
     return (
       <>
-        <button onClick={handlerClick1}>Botão 1</button>
-        <button onClick={handlerClick2}>Botão 2</button>
-        <button onClick={handlerClick3}>Botão 3</button>
+        <button onClick={this.handlerClick1}>Button 1</button>
+        <button onClick={this.handlerClick2}>Button 2</button>
+        <button onClick={this.handlerClick3}>Button 3</button>
       </>
     )
     
