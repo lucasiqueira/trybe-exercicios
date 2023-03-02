4f56116da09be3e1318f9488daf7b1debfe1f4be
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-02 18:23:31 -0300

    feat: atributte odd or even class to button

--- 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Body.jsx
+++ 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Body.jsx
@@ -14,29 +14,41 @@ class Body extends Component {
     clickNumberButton2: 0,
     clickNumberButton3: 0,
   }
+  
+  colorButton = (number, target) => {
+    if (number % 2 === 0) {
+      target.className = 'even'
+      console.log('even');
+    } else {
+      target.className = 'odd';
+      console.log('odd');
+    }
+  }
 
-  handleClick1() {
-    this.setState((prevState, _props) => ({
+  handleClick1 = async (event) => {
+    await this.setState((prevState, _props) => ({
       clickNumberButton1: prevState.clickNumberButton1 + 1,
     }))
+    this.colorButton(this.state.clickNumberButton1, event.target);
   }
-  handleClick2() {
+  handleClick2 = () => {
     this.setState((prevState, _props) => ({
       clickNumberButton2: prevState.clickNumberButton2 + 1,
     }))
   }
-  handleClick3() {
+  handleClick3 = () => {
     this.setState((prevState, _props) => ({
       clickNumberButton3: prevState.clickNumberButton3 + 1,
     }))
   }
+
   
   render() {
     return (
       <>
-        <button onClick={this.handleClick1}>{ this.state.clickNumberButton1 }</button>
-        <button onClick={this.handleClick2}>{ this.state.clickNumberButton2 }</button>
-        <button onClick={this.handleClick3}>{ this.state.clickNumberButton3 }</button>
+        <button onClick={this.handleClick1} id="1">{ this.state.clickNumberButton1 }</button>
+        <button onClick={this.handleClick2} id="2">{ this.state.clickNumberButton2 }</button>
+        <button onClick={this.handleClick3} id="3">{ this.state.clickNumberButton3 }</button>
       </>
     )
     
