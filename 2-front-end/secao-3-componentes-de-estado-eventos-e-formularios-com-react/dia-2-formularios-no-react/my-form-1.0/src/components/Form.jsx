d7ce6181bb5b5d0f3e9f71fd8a062c8bffd5b3e5
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-03 23:04:18 -0300

    feat: controlling all elements

--- 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-1.0/src/components/Form.jsx
+++ 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-1.0/src/components/Form.jsx
@@ -6,43 +6,68 @@ class Form extends Component {
 
     this.state = {
       name: '',
+      email: '',
+      state: 'Select a state',
+      aboutyou: '',
     }
   }
 
-  handleNameChange = ({target}) => {
-    this.setState({ name: target.value })
+  handleChange = ({target}) => {
+    const { name, value } = target
+    this.setState({ [name]: value })
+    console.log(value);
   }
 
   render() {
-    const { name } = this.state;
+    const { name, email, state, aboutyou } = this.state;
     return(
-      <form action="">
-        <label htmlFor="">
-          Selecione qual é o estado em que você mora:
-          <select name="" id="">
-            <option value="">GO</option>
-            <option value="">MT</option>
-            <option value="">MS</option>
-            <option value="">TO</option>
-            <option value="">DF</option>
-          </select>
-        </label>
-        <br />
-        <label htmlFor="">
-          Nome
-          <input type="text" name="name" value={ name } onChange={ this.handleNameChange }/>
-        </label>
-        <br />
-        <label htmlFor="">
-          Sobrenome
-          <input type="text" />
-        </label>
-        <br />
-        <label htmlFor="">
-          Fale sobre você:
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </label>
-      </form>
+      <div>
+        <h1>State and React - Fantastic Technology or reaction to regionalism?</h1>
+        <form>
+          <label htmlFor="">
+            State
+            <select name="state" id="state" value={ state } onChange={ this.handleChange }>
+              <option value="">Select a state</option>
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+
+          </label>
+          <br />
+          <label htmlFor="">
+            Name
+            <input type="text" name="name" value={ name } onChange={ this.handleChange }/>
+          </label>
+          <br />
+          <label htmlFor="">
+            Email
+            <input type="email" name="email" value={ email } onChange={ this.handleChange } />
+          </label>
+          <br />
+          <label htmlFor="">
+            About you:
+            <textarea 
+              name="aboutyou" 
+              id="aboutyou" 
+              cols="30" 
+              rows="10" 
+              value ={ aboutyou }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
+      </div>
     )
   }
 }
