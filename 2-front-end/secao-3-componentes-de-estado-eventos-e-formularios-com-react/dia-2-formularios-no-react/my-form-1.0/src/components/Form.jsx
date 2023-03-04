b95b02c65be46543fd6fa1caa2dd770004710860
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-03-03 23:10:41 -0300

    feat: controlling all elements and changing language

--- 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-1.0/src/components/Form.jsx
+++ 2-front-end/secao-3-componentes-de-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-1.0/src/components/Form.jsx
@@ -24,48 +24,53 @@ class Form extends Component {
       <div>
         <h1>State and React - Fantastic Technology or reaction to regionalism?</h1>
         <form>
-          <label htmlFor="">
-            State
-            <select name="state" id="state" value={ state } onChange={ this.handleChange }>
-              <option value="">Select a state</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
+          <fieldset>
+            <label htmlFor="">
+              State
+              <select name="state" id="state" value={ state } onChange={ this.handleChange }>
+                <option value="">Select a state</option>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+              </select>
 
-          </label>
+            </label>
+            <br />
+            <label htmlFor="">
+              Name
+              <input type="text" name="name" value={ name } onChange={ this.handleChange }/>
+            </label>
+            <br />
+            <label htmlFor="">
+              Email
+              <input type="email" name="email" value={ email } onChange={ this.handleChange } />
+            </label>
+          </fieldset>
           <br />
-          <label htmlFor="">
-            Name
-            <input type="text" name="name" value={ name } onChange={ this.handleChange }/>
-          </label>
-          <br />
-          <label htmlFor="">
-            Email
-            <input type="email" name="email" value={ email } onChange={ this.handleChange } />
-          </label>
-          <br />
-          <label htmlFor="">
-            About you:
-            <textarea 
-              name="aboutyou" 
-              id="aboutyou" 
-              cols="30" 
-              rows="10" 
-              value ={ aboutyou }
-              onChange={ this.handleChange }
-            />
-          </label>
+          <fieldset>
+
+            <label htmlFor="">
+              About you:
+              <textarea 
+                name="aboutyou" 
+                id="aboutyou" 
+                cols="30" 
+                rows="10" 
+                value ={ aboutyou }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </fieldset>
         </form>
       </div>
     )
