95de0e0d6a05edbf510ce357ca945df592ccc14c
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-04-04 00:47:33 -0300

    chore: finishing exercise 1

--- 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-1/main.js
+++ 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-1/main.js
@@ -1,5 +1,26 @@
+import { renderNextColor, renderPreviousColor, renderRandomColor } from "./redux/actions";
+import store from "./redux/store";
 
-const INITIAL_STATE = {
-  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
-  index: 0,
-}
+const previousColorButton = document.getElementById('previous');
+const nextColorButton = document.getElementById('next');
+const randomColorButton = document.getElementById('random');
+const colorValue = document.getElementById('value');
+const container = document.getElementById('container');
+
+nextColorButton.addEventListener('click', () => {
+  store.dispatch(renderNextColor());
+})
+
+previousColorButton.addEventListener('click', () => {
+  store.dispatch(renderPreviousColor());
+})
+
+randomColorButton.addEventListener('click', () => {
+  store.dispatch(renderRandomColor());
+})
+
+store.subscribe(() => {
+  const {colors, index} = store.getState();
+  colorValue.innerHTML = colors[index];
+  container.style.backgroundColor = colors[index];
+})
---
+++ 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-1/redux/actions/index.js
@@ -0,0 +1,23 @@
+export const NEXT_COLOR = 'NEXT_COLOR';
+
+export const renderNextColor = () => {
+  return {
+    type: NEXT_COLOR,
+  }
+};
+
+export const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+
+export const renderPreviousColor = () => {
+  return {
+    type: PREVIOUS_COLOR,
+  }
+};
+
+export const RANDOM_COLOR = 'RANDOM_COLOR';
+
+export const renderRandomColor = () => {
+  return {
+    type: RANDOM_COLOR,
+  }
+};
