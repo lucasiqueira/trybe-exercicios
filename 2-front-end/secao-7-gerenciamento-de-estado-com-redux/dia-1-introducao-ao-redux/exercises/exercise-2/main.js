d96886988263edfd0b631abae3bec5d6918f6f82
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-04-04 01:12:42 -0300

    chore: finishing exercise 2

--- 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-2/main.js
+++ 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-2/main.js
@@ -1,15 +1,25 @@
-const INITIAL_STATE = {
-  status: 'offline',
-  theme: 'dark',
-};
+import { changeDisponibility, changeThemeType } from "./redux/actions";
+import store from "./redux/store";
 
 const themeButton = document.getElementById('toggle-theme');
 const statusButton = document.getElementById('toggle-status');
 
+const body = document.getElementById('body');
+const status = document.getElementById('status');
+
 themeButton.addEventListener('click', () => {
-  // coloque o código aqui.
+  store.dispatch(changeThemeType());
 });
 
 statusButton.addEventListener('click', () => {
-  // coloque o código aqui.
+  store.dispatch(changeDisponibility());
 });
+
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+  body.style.backgroundColor = (state.theme === 'dark') ? '#333' : 'white';
+  body.style.color = (state.theme === 'dark') ? 'white' : '#333';
+  const statusString = `${state.status[0].toUpperCase()}${state.status.slice(1)}`
+  status.innerHTML = statusString;
+})
---
+++ 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercises/exercise-2/redux/actions/index.js
@@ -0,0 +1,15 @@
+export const CHANGE_THEME = 'CHANGE_THEME';
+
+export const changeThemeType = () => {
+  return {
+    type: CHANGE_THEME,
+  }
+}
+
+export const CHANGE_DISPONIBILITY = 'CHANGE_DISPONIBILITY';
+
+export const changeDisponibility = () => {
+  return {
+    type: CHANGE_DISPONIBILITY,
+  }
+}
