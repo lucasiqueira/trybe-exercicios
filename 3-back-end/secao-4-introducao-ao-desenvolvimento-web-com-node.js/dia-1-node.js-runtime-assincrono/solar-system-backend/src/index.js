526236f28c8fd47a11ccf2db90be34660ac61987
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-06-19 21:33:49 -0300

    chore: solar system - write in json

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/solar-system-backend/src/index.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/solar-system-backend/src/index.js
@@ -1,9 +1,19 @@
-const { readMissionsData } = require("./utils/fsUtils");
-
+const readline = require('readline-sync');
+const { writeNewMissionData } = require('./utils/fsUtils');
 
 async function main() {
-  const missions = await readMissionsData();
-  console.log(missions);
+  const name = readline.question('Qual é o nome da missão? ');
+  const year = readline.question('Qual foi o ano da missão? ');
+  const country = readline.question('Qual foi o país responsável pela missão? ');
+  const destination = readline.question('Qual foi o destino da missão? ');
+  const newMission = {
+    name,
+    year,
+    country,
+    destination,
+  };
+  writeNewMissionData(newMission);
+  console.log('Missão registrada com sucesso!');
 }
 
 main();
--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/solar-system-backend/src/utils/fsUtils.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/solar-system-backend/src/utils/fsUtils.js
@@ -1,9 +1,10 @@
 const fs = require('fs').promises;
 const path = require('path');
+const MISSION_DATA_PATH = '../../data/missions.json'
 
 async function readMissionsData() {
   try {
-    const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'));
+    const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
     const missions = JSON.parse(data);
     return missions;
   } catch (error) {
@@ -11,4 +12,20 @@ async function readMissionsData() {
   }
 }
 
-module.exports = {readMissionsData};
+async function writeNewMissionData(newMission) {
+  try {
+    const oldMissions = await readMissionsData();
+    const allMissions = JSON.stringify([
+      ...oldMissions,
+      {id: Date.now(), ...newMission}
+    ]);
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions)
+  } catch (error) {
+    console.error(`Erro na escrita do arquivo: ${error}`);
+  }
+}
+
+module.exports = {
+  readMissionsData,
+  writeNewMissionData
+};
