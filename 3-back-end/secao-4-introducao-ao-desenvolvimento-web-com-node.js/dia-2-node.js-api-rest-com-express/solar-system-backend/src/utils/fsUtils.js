5d52a540304e7bd9fe82474d0494155543215cb9
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-06-21 00:02:25 -0300

    chore: creating API methods for solar-system exercise

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-2-node.js-api-rest-com-express/solar-system-backend/src/utils/fsUtils.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-2-node.js-api-rest-com-express/solar-system-backend/src/utils/fsUtils.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
-const MISSION_DATA_PATH = '../../data/missions.json'
+
+const MISSION_DATA_PATH = '../../data/missions.json';
 
 async function readMissionsData() {
   try {
@@ -15,17 +16,52 @@ async function readMissionsData() {
 async function writeNewMissionData(newMission) {
   try {
     const oldMissions = await readMissionsData();
-    const allMissions = JSON.stringify([
-      ...oldMissions,
-      {id: Date.now(), ...newMission}
-    ]);
-    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions)
+    const newMissionWithId = { id: Date.now(), ...newMission };
+    const allMissions = JSON.stringify([...oldMissions, newMissionWithId]);
+
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions);
+    return newMissionWithId;
   } catch (error) {
     console.error(`Erro na escrita do arquivo: ${error}`);
   }
 }
 
+async function updateMissionData(id, updatedMissionData) {
+  const oldMissions = await readMissionsData();
+  const updatedMission = { id, ...updatedMissionData };
+  const updatedMissions = oldMissions.reduce((missionsList, currentMission) => {
+    if (currentMission.id === updatedMission.id) return [...missionsList, updatedMission];
+    return [...missionsList, currentMission];
+  }, []);
+
+  const updateData = JSON.stringify(updatedMissions);
+
+  try {
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), updateData);
+    console.log(`Atualizou a missão com o id ${id}`);
+    return updatedMission;
+  } catch (error) {
+    console.error(`Erro na atualização do arquivo: ${error}`);
+  }
+}
+
+async function deleteMissionsData(id) {
+  const oldMissions = await readMissionsData();
+  const updatedMissions = oldMissions.filter((curr) => curr.id !== id);
+
+  const updatedData = JSON.stringify(updatedMissions);
+
+  try {
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), updatedData);
+    console.log(`Deletou a missão com o id ${id}`);
+  } catch (error) {
+    console.error(`Erro na deleção da missão: ${error}`);
+  }
+}
+
 module.exports = {
   readMissionsData,
-  writeNewMissionData
+  writeNewMissionData,
+  updateMissionData,
+  deleteMissionsData,
 };
