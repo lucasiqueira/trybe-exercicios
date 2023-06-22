eb281c4fecfa7f5fe46038a49b32c3c22ab11ce4
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-06-22 01:21:14 -0300

    chore: creating middlewares - solar system

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-4-node-js-express-e-middlewares/solar-system-backend/src/app.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-4-node-js-express-e-middlewares/solar-system-backend/src/app.js
@@ -10,20 +10,41 @@ const app = express();
 
 app.use(express.json());
 
+const validateMissionId = (req, res, next) => {
+  const { id } = req.params;
+
+  const idAsNumber = Number(id);
+  if (Number.isNaN(idAsNumber)) {
+    res.status(400).send({ message: 'ID inválido. Precisa ser um número.' });
+  } else {
+    next();
+  }
+};
+
+const validateMissionData = (req, res, next) => {
+  const requiredProperties = ['name', 'year', 'country', 'destination'];
+
+  if (requiredProperties.every((property) => property in req.body)) {
+    next();
+  } else {
+    res.status(400).send({ message: 'A missão não possui todos os atributos necessários.' });
+  }
+};
+
 app.get('/missions', async (req, res) => {
   const missions = await readMissionsData();
 
   return res.status(200).json({ missions });
 });
 
-app.post('/missions', async (req, res) => {
+app.post('/missions', validateMissionData, async (req, res) => {
   const newMission = req.body;
 
   const newMissionWithId = await writeNewMissionData(newMission);
   return res.status(201).json({ mission: newMissionWithId });
 });
 
-app.put('/missions/:id', async (req, res) => {
+app.put('/missions/:id', validateMissionId, validateMissionData, async (req, res) => {
   const { id } = req.params;
   const updatedMissionData = req.body;
 
@@ -32,7 +53,7 @@ app.put('/missions/:id', async (req, res) => {
   return res.status(200).json({ mission: updatedMission });
 });
 
-app.delete('/missions/:id', async (req, res) => {
+app.delete('/missions/:id', validateMissionId, async (req, res) => {
   const { id } = req.params;
   await deleteMissionsData(Number(id));
 
