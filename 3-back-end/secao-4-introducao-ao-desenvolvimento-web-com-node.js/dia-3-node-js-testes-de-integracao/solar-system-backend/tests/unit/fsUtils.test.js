5a252528ecd22d3fcc67caff5d916b4778c918df
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-06-22 00:33:03 -0300

    test: improving tests with sinon lib

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-3-node-js-testes-de-integracao/solar-system-backend/tests/unit/fsUtils.test.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-3-node-js-testes-de-integracao/solar-system-backend/tests/unit/fsUtils.test.js
@@ -1,10 +1,21 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
 
-const {readMissionsData} = require('../../src/utils/fsUtils');
+const { readMissionsData } = require('../../src/utils/fsUtils');
 
-describe('A função readMissionsData', function() {
-  it('retorna um array', async function() {
+const mockMissions = JSON.stringify([
+  { id: 1, name: 'James Webb', year: '2021', country: 'EUA', destination: 'Espaço Interestelar' },
+  { id: 2, name: 'Myke Jokes', year: '2022', country: 'URSS', destination: 'Espaço Interestelar' },
+  { id: 3, name: 'Martha Wilk', year: '2023', country: 'Japão', destination: 'Marte' },
+]);
+
+describe('A função readMissionsData', function () {
+  it('retorna um array com todos os elementos do arquivo json', async function () {
+    sinon.stub(fs.promises, 'readFile').resolves(mockMissions);
     const missions = await readMissionsData();
     expect(missions).to.be.instanceOf(Array);
-  })
-})
+    expect(missions).to.have.lengthOf(3);
+    sinon.restore();
+  });
+});
