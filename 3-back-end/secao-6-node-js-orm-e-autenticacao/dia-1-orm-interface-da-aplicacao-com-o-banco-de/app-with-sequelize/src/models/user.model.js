b8776d8891b928991bb30df5a873039cfb0e451b
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-08-05 11:24:18 -0300

    chore: exercises dia 6.1

---
+++ 3-back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,15 @@
+// src/models/user.model.js
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+  (async () => {
+    await sequelize.sync({ force: true });
+    // As funções vão aqui
+  })();
+  return User;
+};
+
+module.exports = UserModel;
