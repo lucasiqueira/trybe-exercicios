e81905e5b002e73d01427b057e55936111afadc5
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-10-02 17:14:51 -0300

    feat: implements double linked list class -2

--- 4-ciencia-da-computacao/secao-3-estruturas-de-dados-listas-lineares/dia-3-no-e-listas-encadeadas/src/double_linked_list.py
+++ 4-ciencia-da-computacao/secao-3-estruturas-de-dados-listas-lineares/dia-3-no-e-listas-encadeadas/src/double_linked_list.py
@@ -29,3 +29,56 @@ class DoubleLinkedList:
         last_value.previous = self.tail_value
         self.tail_value = last_value
         self.__length += 1
+
+    def insert_at(self, value, position):
+        if position < 1:
+            return self.insert_first(value)
+        if position >= len(self):
+            return self.insert_last(value)
+        new_value = Node(value)
+        previous_value = self.head_value
+        for _ in range(position - 1):
+            previous_value = previous_value.next
+        new_value.next = previous_value.next
+        new_value.previous = previous_value
+        previous_value.next = new_value
+        self.__length += 1
+
+    def remove_first(self):
+        if not self.head_value:
+            return None
+        if self.head_value.next:
+            self.head_value.next.previous = None
+        self.head_value = self.head_value.next
+        self.__length -= 1
+
+    def remove_last(self):
+        if not self.tail_value:
+            return None
+        if self.tail_value.previous:
+            self.tail_value.previous.next = None
+        self.tail_value = self.tail_value.previous
+        self.__length -= 1
+
+    def remove_at(self, position):
+        if position < 1:
+            return self.remove_first()
+        if position >= len(self):
+            return self.remove_last()
+        previous_value = self.head_value
+        for _ in range(position - 1):
+            previous_value = previous_value.next
+        previous_value.next = previous_value.next.next
+        previous_value.next.previous = previous_value
+        self.__length -= 1
+
+    def get_element_at(self, position):
+        if position < 0 or position >= len(self):
+            return None
+        current_value = self.head_value
+        for _ in range(position):
+            current_value = current_value.next
+        return current_value.value
+
+    def is_empty(self):
+        return self.head_value is None
