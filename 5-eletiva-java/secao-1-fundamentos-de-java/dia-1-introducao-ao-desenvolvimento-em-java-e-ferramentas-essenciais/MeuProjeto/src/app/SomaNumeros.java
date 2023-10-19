03b8946d2d1683f55f85f634a63feebd65796bf6
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-10-19 11:38:26 -0300

    chore: first java exercises

---
+++ 5-eletiva-java/secao-1-fundamentos-de-java/dia-1-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuProjeto/src/app/SomaNumeros.java
@@ -0,0 +1,16 @@
+package app;
+
+public class SomaNumeros {
+
+  /**
+   * Retorna a soma entre dois números.
+   *
+   * @param numA um número inteiro.
+   * @param numB outro número inteiro
+   * @return a soma entre os dois números.
+   */
+  public static int retornaSomaEntreDoisNumeros(int numA, int numB) {
+    int soma = numA + numB;
+    return soma;
+  }
+}
