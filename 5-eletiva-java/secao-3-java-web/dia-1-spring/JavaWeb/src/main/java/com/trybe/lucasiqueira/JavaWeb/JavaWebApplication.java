daa55dbee3da4e87c84b4504f21f92bc4e2ea494
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-11-01 00:38:49 -0300

    chore: java spring first exercise

---
+++ 5-eletiva-java/secao-3-java-web/dia-1-spring/JavaWeb/src/main/java/com/trybe/lucasiqueira/JavaWeb/JavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.lucasiqueira.JavaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+}
