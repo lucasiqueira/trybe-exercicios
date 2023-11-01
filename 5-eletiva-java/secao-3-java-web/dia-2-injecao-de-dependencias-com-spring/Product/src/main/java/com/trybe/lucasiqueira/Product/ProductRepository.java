6f19d68c5c4c7e238e14287854d8605493153c55
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-11-01 19:41:04 -0300

    chore: adds config file do application

--- 5-eletiva-java/secao-3-java-web/dia-2-injecao-de-dependencias-com-spring/Product/src/main/java/com/trybe/lucasiqueira/Product/ProductRepository.java
+++ 5-eletiva-java/secao-3-java-web/dia-2-injecao-de-dependencias-com-spring/Product/src/main/java/com/trybe/lucasiqueira/Product/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.trybe.lucasiqueira.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
+
+  private Database database;
+
+  @Autowired
+  public ProductRepository(Database database) {
+    this.database = database;
+  }
 
   public String getMessage() {
     return "Mensagem: produto retornado";
