3acc6b9b448dda150b421dd7dbc0e751af7d5a6b
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-11-18 17:19:39 -0300

    chore: implements alexandria books routes

--- 5-eletiva-java/secao-4-persistencia-i/dia-2-spring-data-jpa/alexandria/src/main/java/com/trybe/alexandria/models/entities/Book.java
+++ 5-eletiva-java/secao-4-persistencia-i/dia-2-spring-data-jpa/alexandria/src/main/java/com/trybe/alexandria/models/entities/Book.java
@@ -1,7 +1,17 @@
 package com.trybe.alexandria.models.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
   private String genre;
---
+++ 5-eletiva-java/secao-4-persistencia-i/dia-2-spring-data-jpa/alexandria/src/main/java/com/trybe/alexandria/services/BookService.java
@@ -0,0 +1,55 @@
+package com.trybe.alexandria.services;
+
+import com.trybe.alexandria.models.entities.Book;
+import com.trybe.alexandria.models.repositories.BookRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+  private BookRepository bookRepository;
+
+  @Autowired
+  public BookService(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  public Book insertBook(Book book) {
+    return bookRepository.save(book);
+  }
+
+  public Optional<Book> updateBook(Long id, Book book) {
+    Optional<Book> optionalBook = bookRepository.findById(id);
+
+    if (optionalBook.isPresent()) {
+      Book bookFromDB = optionalBook.get();
+      bookFromDB.setTitle(book.getTitle());
+      bookFromDB.setGenre(book.getGenre());
+
+      Book updatedBook = bookRepository.save(bookFromDB);
+      return Optional.of(updatedBook);
+    }
+    return optionalBook;
+  }
+
+  public Optional<Book> removeBookById(Long id) {
+    Optional<Book> bookOptional = bookRepository.findById(id);
+
+    if (bookOptional.isPresent()) {
+      bookRepository.deleteById(id);
+    }
+
+    return bookOptional;
+  }
+
+  public Optional<Book> getBookById(Long id) {
+    return bookRepository.findById(id);
+  }
+
+  public List<Book> getAllBooks() {
+    return bookRepository.findAll();
+  }
+}
