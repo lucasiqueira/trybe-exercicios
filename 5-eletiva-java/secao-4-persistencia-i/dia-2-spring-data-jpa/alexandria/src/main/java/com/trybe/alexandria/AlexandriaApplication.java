107ef1f5712aac3d00c2f56a2b3c2f223aabb6fc
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-11-18 15:26:51 -0300

    chore: alexandria api first steps whit spring data jpa

---
+++ 5-eletiva-java/secao-4-persistencia-i/dia-2-spring-data-jpa/alexandria/src/main/java/com/trybe/alexandria/AlexandriaApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.alexandria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandriaApplication.class, args);
+	}
+
+}
